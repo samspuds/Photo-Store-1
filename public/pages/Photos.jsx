29aad99e7112fd7e88093c29e86ba7dfaf4db428
samspuds <90201674+samspuds@users.noreply.github.com>
2023-02-09 16:05:36 +0000

    Setting up basic structure of app

---
+++ public/pages/Photos.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Photos(){
+    return (
+            <div>a</div>
+
+    )
+}
+
+export default Photos
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import React from "react"
+import Header from "../public/components/Header"
+import Cart from "../public/pages/Cart"
+import Photos from "../public/pages/Photos"
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
     </div>
   )
 }
