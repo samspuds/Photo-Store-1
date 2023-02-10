13337fd871bc530192282ec9900ac6bedfe8d769
samspuds <90201674+samspuds@users.noreply.github.com>
2023-02-10 15:30:14 +0000

    set up useEffect to retrieve photos from API call
    photos stored in allPhotos array

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,6 +4,13 @@ const Context = React.createContext()
 
 function ContextProvider(props){
     const[allPhotos, setAllPhotos] = useState([])
+
+    React.useEffect(() => {
+        fetch(`https://picsum.photos/v2/list?page=9&limit=99`)
+        .then(res => res.json())
+        .then(data => setAllPhotos(data))
+    }, [])
+
     return (
         <Context.Provider value={{allPhotos}}>
             {props.children}
