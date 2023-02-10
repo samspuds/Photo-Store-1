ba19d8c35b3f0f1626f9a8552e99c2cd6a12e534
samspuds <90201674+samspuds@users.noreply.github.com>
2023-02-10 14:57:39 +0000

    setting up state
    to allow the app to access photos

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React, {useState} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider(props){
+    const[allPhotos, setAllPhotos] = useState([])
     return (
-        <Context.Provider>
+        <Context.Provider value={{allPhotos}}>
             {props.children}
         </Context.Provider>
     )
